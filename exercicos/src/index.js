54277fbb5650c873d9badc1fca91d7785f4c9384
BRUNO FERNANDO LACERDA DE OLIVEIRA <57118407+LacerdaBruno@users.noreply.github.com>
2021-06-05 14:28:53 -0300

    05/06/2021

---
+++ exercicos/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/BomDia'
+
+ReactDOM.render(<Primeiro/>, document.getElementById('root'))
+
+ReactDOM.render(<BomDia nome = "Bruno"/>, document.getElementById('root'))
